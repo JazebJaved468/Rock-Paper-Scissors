3c53de13feecfc5bd815f9c71713c0eed4cfae99
Jazeb_Javed <jazebjaved52@gmail.com>
2023-04-09 20:22:19 +0500

    4 bugs removed successfully, including null error, empty string etc

--- index.js
+++ index.js
@@ -3,19 +3,30 @@ const P = "paper";
 const S = "scissors";
 
 
+
 function userInput(){
     let input = prompt("write input rock,paper or scissors");
-    // let input = "RocK";
-    let user_input = input.toLowerCase();
-    // return(userInput);
 
-    // Artificial Intelligence
-    let suggested_input = user_input.slice(0) === "r" ? "rock" : user_input.slice(0,2) === "p" ? "paper" : user_input.slice(0,2) === "s" ? "scissors" : console.log("Wrong input");
-    //
-    return (suggested_input);
+    // console.log(input);             // for debugging
+
+    if(input == undefined){
+        return ("cancelled");
+    }
+    else{
+        let trimmed_input = input.trim();
+        let user_input = trimmed_input.toLowerCase();
+        // return(userInput);
+
+        // Artificial Intelligence
+        let suggested_input = user_input[0] === "r" ? "rock" : user_input[0] === "p" ? "paper" : user_input[0]=== "s" ? "scissors" : "wrong_input";
+        //
+        return (suggested_input);
+    }
+    
 }
 
 function computerInput(){
+
     let computer_options = ["rock", "paper", "scissors"]
     let random_index = Math.floor(Math.random() * computer_options.length);
     return(computer_options[random_index]);
@@ -23,21 +34,41 @@ function computerInput(){
 }
 
 function play(user, computer){
-    console.log("user =", user,);
-    console.log("computer =", computer);
-    if(user === computer){
-        return("tie");
+    // console.log("user =", user,);                // for debugging
+    // console.log("computer =", computer);
+
+    if(user === "cancelled" ){
+        return undefined;                           // by default return returns undefined
     }
-    else if( user === R && computer === S || user === S && computer === P || user === P && computer === R){
-        return("userwin");
+
+    else if (user === "wrong_input"){
+        return ("again");
     }
     else{
-        return("computerwin");
+        if(user === computer){
+            return("tie");
+        }
+        else if( user === R && computer === S || user === S && computer === P || user === P && computer === R){
+            return("userwin");
+        }
+        else{
+            return("computerwin");
+        }
     }
+    
 }
 
 function congrats(userScore, computerScore){
-    (userScore === 5) ? console.log("Congrats!! You win") : console.log("COngrats!! You lost") ;
+    (userScore === 5) ? console.log("Congrats!! You win") : console.log("Sedd!! You lost") ;
+}
+
+function gameEnd(userScore, computerScore){ 
+    if(userScore === 5 || computerScore === 5){
+        congrats(userScore, computerScore);
+    }
+    else{
+        console.log("Game remains unfinished :(")
+    }
 }
 
 function game(){
@@ -48,23 +79,35 @@ function game(){
         console.log(`=====================Round ${round} ========================`);
         let result = play(userInput(), computerInput());
 
-        if(result === "userwin"){
-            userScore++;
-            console.log("you beats computer");
+        if(result == undefined){
+            alert("you cancelled the game")
+            break;
         }
-        else if(result === "computerwin"){
-            computerScore++;
-            console.log("computer beats you");
+
+        else if(result === "again"){
+            continue;
         }
         else{
-            console.log(result);
+            if(result === "userwin"){
+                userScore++;
+                console.log("you beats computer");
+            }
+            else if(result === "computerwin"){
+                computerScore++;
+                console.log("computer beats you");
+            }
+            else{
+                console.log(result);  // here result = "tie"
+            }
+            ++round;
         }
-        ++round;
     }
 
-    console.log(`user = ${userScore} \n computer = ${computerScore} \n user + computer = ${userScore + computerScore}`);
+    gameEnd(userScore, computerScore);
 
-    congrats(userScore, computerScore);
+    // console.log(`user = ${userScore} \n computer = ${computerScore} \n user + computer = ${userScore + computerScore}`); // for debugging
+
+    
 }
 
 const playgame = document.querySelector('button');
